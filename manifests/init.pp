7c03f0348cd6415e274cf08f5e96174d7a270514
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-10-31 15:59:18 -0400

    initial code

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+class formation_extra (
+  Array[Struct[{ filename => String[1], source => String[1] }]] $stripped_skel_archives = [],
+) {
+
+  ensure_resource('file', '/opt/puppetlabs/puppet/cache/puppet-archive', { 'ensure' => 'directory' })
+  $stripped_skel_archives.each |$index, Hash $archive| {
+    $filename = $archive['filename']
+    archive { "skel_${index}":
+      path            => "/opt/puppetlabs/puppet/cache/puppet-archive/${filename}",
+      extract         => true,
+      extract_path    => '/etc/skel.ipa',
+      extract_command => 'tar xf %s --strip-components=1',
+      source          => $archive['source'],
+      require         => File['/etc/skel.ipa'],
+      notify          => Exec['chown -R root:root /etc/skel.ipa'],
+    }
+  }
+}
