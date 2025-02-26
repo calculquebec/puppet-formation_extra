df79b9db68eab81c2de6a5cf4fe9e1d230202d1f
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-02-26 09:46:35 -0500

    add support for is_event_soon.py

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,8 @@
 class formation_extra (
   Array[Struct[{ filename => String[1], source => String[1] }]] $stripped_skel_archives = [],
+  String google_calendar_id,
+  String google_api_key,
+  String timezone = "UTC",
 ) {
 
   ensure_resource('file', '/opt/puppetlabs/puppet/cache/puppet-archive', { 'ensure' => 'directory' })
@@ -14,5 +17,27 @@ class formation_extra (
       require         => File['/etc/skel.ipa'],
       notify          => Exec['chown -R root:root /etc/skel.ipa'],
     }
+  }
+
+  file { '/usr/sbin/is_event_soon.py':
+    path   => '/usr/sbin/is_event_soon.py'
+    ensure => 'file'
+    mode   => 'u+rx,go-rwx'
+    source => 'https://raw.githubusercontent.com/calculquebec/CQORC/refs/heads/main/is_event_soon.py'
+    owner  => 'root'
+  }
+
+  file { '/etc/cq_event_check.ini':
+    path    => '/etc/cq_event_check.ini'
+    ensure  => 'file'
+    mode    => 'u+rwx,go-rwx'
+    owner   => 'root'
+    content => @("EOF"),
+      # FILE MANAGED BY PUPPET, DO NOT EDIT DIRECTLY
+      [DEFAULT]
+      calendar_id = ${google_calendar_id}
+      apikey = ${google_api_key}
+      timezone = ${timezone}
+      |EOF
   }
 }
